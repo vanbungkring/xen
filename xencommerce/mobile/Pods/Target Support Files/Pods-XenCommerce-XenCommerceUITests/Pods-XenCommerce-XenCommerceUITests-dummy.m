6c99505942f1d7b303f68bd067670ddaa51b711d
vanbungkring <prasetyo.mailbox@gmail.com>
2021-01-20 14:26:16 +0700

    init mobile apps

---
+++ xencommerce/mobile/Pods/Target Support Files/Pods-XenCommerce-XenCommerceUITests/Pods-XenCommerce-XenCommerceUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XenCommerce_XenCommerceUITests : NSObject
+@end
+@implementation PodsDummy_Pods_XenCommerce_XenCommerceUITests
+@end
---
+++ xencommerce/mobile/Pods/Target Support Files/Pods-XenCommerce/Pods-XenCommerce-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XenCommerce : NSObject
+@end
+@implementation PodsDummy_Pods_XenCommerce
+@end
